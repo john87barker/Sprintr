144681b9f66ede7103f687b9879fb8c415c280d1
john87barker <john87barker@gmail.com>
2021-08-09 19:52:28 -0600

    img and colors

--- Sprintr.client/src/pages/HomePage.vue
+++ Sprintr.client/src/pages/HomePage.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center">
-    <img src="https://bcw.blob.core.windows.net/public/img/8600856373152463" alt="CodeWorks Logo">
+    <img src="../assets/img/run-fast.png" alt="Sprintr Logo">
     <h1 class="my-5 bg-dark text-light p-3 rounded d-flex align-items-center">
-      <span class="mx-2 text-white">Vue 3 Starter</span>
+      <span class="mx-2 text-white">Sprintr</span>
     </h1>
   </div>
 </template>
